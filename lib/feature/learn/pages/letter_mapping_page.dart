7e20f0b44b7274956fda08385d0b5c39116abbfa
jagdish bhai <jagdissghimire@gmail.com>
2024-12-03 18:20:22 +0545

    chore: learn mapping page is created

---
+++ lib/feature/learn/pages/letter_mapping_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class LetterMappingPage extends StatelessWidget {
+  const LetterMappingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.deepPurple.shade100,
+          title: Text("Letter Mapping"),
+        ),
+        body: ListView(
+          children: [],
+        ));
+  }
+}
