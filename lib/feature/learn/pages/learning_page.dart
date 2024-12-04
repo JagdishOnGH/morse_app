157caf840746be4b8f3b555fdee2ca5fd3b78110
jagdish bhai <jagdissghimire@gmail.com>
2024-12-04 17:30:52 +0545

    chore: new learning method added

--- lib/feature/learn/pages/learning_page.dart
+++ lib/feature/learn/pages/learning_page.dart
@@ -23,6 +23,60 @@ class LearningPage extends StatelessWidget {
               ),
             ),
           ),
+          SizedBox(height: 20),
+          //"Morse code of A is
+          Padding(
+            padding: const EdgeInsets.only(left: 28.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Text("Morse Value",
+                    style: Theme.of(context).textTheme.headlineSmall?.copyWith(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.deepPurple)),
+                SizedBox(width: 40),
+                Chip(
+                  label: Text(".-",
+                      style: Theme.of(context)
+                          .textTheme
+                          .headlineMedium
+                          ?.copyWith(
+                              fontWeight: FontWeight.w600,
+                              color: Colors.deepPurple)),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 40),
+          Padding(
+            padding: const EdgeInsets.only(left: 28.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Text("Pronounced as",
+                    style: Theme.of(context).textTheme.headlineSmall?.copyWith(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.deepPurple)),
+                SizedBox(width: 40),
+                Chip(
+                  label: Text("Di-Dah",
+                      style: Theme.of(context)
+                          .textTheme
+                          .headlineSmall
+                          ?.copyWith(
+                              fontSize: 20,
+                              fontWeight: FontWeight.w600,
+                              color: Colors.deepPurple)),
+                ),
+              ],
+            ),
+          ),
+
+          SizedBox(height: 40),
+          Divider(),
+          Text("Quizzes are coming soon..."),
         ],
       ),
     );
