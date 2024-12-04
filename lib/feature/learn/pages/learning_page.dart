6012568cd1301491cff2e4710df495546d462e63
jagdish bhai <jagdissghimire@gmail.com>
2024-12-04 17:30:26 +0545

    chore: hero animation added

--- lib/feature/learn/pages/learning_page.dart
+++ lib/feature/learn/pages/learning_page.dart
@@ -15,34 +15,13 @@ class LearningPage extends StatelessWidget {
             decoration: BoxDecoration(
               color: Colors.deepPurple.shade100,
             ),
-          SizedBox(height: 50),
-
-          // Drag Target Container
-          DragTarget<String>(
-            builder: (context, candidateData, rejectedData) {
-              print(rejectedData);
-              return Container(
-                width: 200,
-                height: 100,
-                color: candidateData.isEmpty
-                    ? Colors.grey[300]
-                    : Colors.green[200],
-                child: Center(
-                  child: Text(
-                    isChipDropped ? "Chip Dropped!" : "Drop here",
-                    style: TextStyle(fontSize: 18),
-                  ),
-                ),
-              );
-            },
-            onWillAcceptWithDetails: (data) {
-              return data.data != "Hello" ? false : true;
-            },
-            onAcceptWithDetails: (data) {
-              setState(() {
-                isChipDropped = true;
-              });
-            },
+            child: Hero(
+              tag: "alphabets",
+              child: Text(
+                "A",
+                style: TextStyle(fontWeight: FontWeight.w800, fontSize: 100),
+              ),
+            ),
           ),
         ],
       ),
