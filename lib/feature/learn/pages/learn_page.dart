9e5a09bd2e59adcf486e3a44ee7744b6ea62cae3
jagdish bhai <jagdissghimire@gmail.com>
2024-12-07 20:34:36 +0545

    chore: added new pages

--- lib/feature/learn/pages/learn_page.dart
+++ lib/feature/learn/pages/learn_page.dart
@@ -66,8 +66,9 @@ class LearnPage extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   Text("Morse To Numbers",
-                      style:
-                          ts.titleLarge?.copyWith(fontWeight: FontWeight.w600)),
+                      style: ts.titleLarge?.copyWith(
+                          fontWeight: FontWeight.w600,
+                          color: Colors.grey.shade800)),
                   InkWell(
                       onTap: () {
                         // goto learning page
